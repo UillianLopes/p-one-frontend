8103ce7bf041403e101d99e8a4c11dad8c0b7712
UillianLopes <uilliansl@outlook.com>
2022-08-16 17:37:52 -0300

    Remove role

--- libs/features/admin/profiles/src/lib/profile-details/profile-details-role/profile-details-role.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'li[p-one-profile-details-role]',
-  templateUrl: './profile-details-role.component.html',
-  styleUrls: ['./profile-details-role.component.scss'],
-})
-export class ProfileDetailsRoleComponent {}
